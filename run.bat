c67728ede6de0adea27ecc942b3df6256aa2b18c
Victor Nicollet <victor.nicollet@lokad.com>
2013-09-25 11:16:30 +0200

    Drop VS, use batch file

---
+++ run.bat
@@ -0,0 +1,3 @@
+SET PATH=%PATH%;C:\Program Files\PortableJekyll\ruby\bin;C:\Program Files\PortableJekyll\devkit\bin;C:\Program Files\PortableJekyll\git\bin;C:\Program Files\PortableJekyll\Python\App
+jekyll.bat serve --watch
+pause
